7a85cead7424925dad74ff838fd904fbf0ded5a8
Maurits Hameter <maurits-hameter@hotmail.com>
2020-12-02 13:31:42 +0100

    solution day 2

--- day2/src/aoc2020/Main.java
+++ day2/src/aoc2020/Main.java
@@ -2,26 +2,41 @@ package aoc2020;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList<Password> passwordList = new ArrayList<>();
+
         try {
             File file = new File("input.txt");
             Scanner scanner = new Scanner(file);
             while(scanner.hasNextLine()) {
                 String temp = scanner.nextLine();
                 String info[] = temp.split("(-|:|\\s)");
-                System.out.println(Arrays.toString(info));
-                break;
+                int min = Integer.parseInt(info[0]);
+                int max = Integer.parseInt(info[1]);
+                char letter = info[2].charAt(0);
+                String password = info[4];
+                Password entry = new Password(password, min, max, letter);
+                passwordList.add(entry);
                 //process the input file here
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        int count = 0;
+        for (Password i : passwordList) {
+            if (i.isValidPasswordV2()) {
+                count++;
+            }
+        }
+        System.out.println(count);
 
 
     }
--- day2/src/aoc2020/Password.java
+++ day2/src/aoc2020/Password.java
@@ -3,11 +3,36 @@ package aoc2020;
 public class Password {
 
     String password;
-    char condition[] = new char[3]; // array with conditions for valid password. {min, max, character}
+    int min, max; //minimal and maximal occurence of letter in password to be valid
+    char letter;
 
-    public Password(String password, char condition[]) {
+
+    public Password(String password, int min, int max, char letter) {
         this.password = password;
-        this.condition = condition;
+        this.min = min;
+        this.max = max;
+        this.letter = letter;
+    }
+
+    public boolean isValidPassword() {
+        int occurance = countCharacter(letter);
+        if (occurance >= min && occurance <= max) {
+            return true;
+        }
+        return false;
+    }
+
+    public int countCharacter(char letter) {
+        return (int) password.chars().filter(ch -> ch == letter).count();
+    }
+
+    public boolean isValidPasswordV2() {
+       boolean letterAtMin = password.charAt(min -1) == letter;
+       boolean letterAtMax = password.charAt(max-1) == letter;
+       if (letterAtMin && !letterAtMax || !letterAtMin && letterAtMax) {
+           return true;
+       }
+       return false;
     }
 
 }
