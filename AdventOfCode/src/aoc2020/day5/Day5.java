9b22cbf35892454ae549d4f49421cbd39f00f67d
Maurits Hameter <maurits-hameter@hotmail.com>
2020-12-03 10:17:51 +0100

    template for day4 and 5

---
+++ AdventOfCode/src/aoc2020/day5/Day5.java
@@ -0,0 +1,19 @@
+package aoc2020.day5;
+
+import aoc2020.Util;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Day5 {
+    public static void run() {
+        File file = new File("day3.txt");
+        ArrayList<String/*NewObject*/> newObjectList = new ArrayList<>(); //keep track of the processed objects
+        ArrayList<String> input = Util.readFile(file);
+        for (String s: input) {
+            //process the input file into whatever we need
+        }
+        int res = 0; // STORE RESULT
+        System.out.println(res); //OUTPUT RESULT
+    }
+}
