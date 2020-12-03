d07214152626abfacd7a26d8035aae1d15dfbe9c
Maurits Hameter <maurits-hameter@hotmail.com>
2020-12-03 10:17:07 +0100

    Solution for day 3

--- AdventOfCode/src/aoc2020/Main.java
+++ AdventOfCode/src/aoc2020/Main.java
@@ -2,10 +2,36 @@ package aoc2020;
 
 import aoc2020.day1.Day1;
 import aoc2020.day2.Day2;
+import aoc2020.day3.Day3;
+import aoc2020.day4.Day4;
 
 public class Main {
 
     public static void main(String[] args) {
-	Day2.run(); //run day1
+//        Day1.run(); //run day1
+//        Day2.run(); //run day2
+//        Day3.run(); //run day3
+        Day4.run(); //run day4
+//        Day5.run(); //run day5
+//        Day6.run(); //run day6
+//        Day7.run(); //run day7
+//        Day8.run(); //run day8
+//        Day9.run(); //run day9
+//        Day10.run(); //run day10
+//        Day11.run(); //run day11
+//        Day12.run(); //run day12
+//        Day13.run(); //run day13
+//        Day14.run(); //run day14
+//        Day15.run(); //run day15
+//        Day16.run(); //run day16
+//        Day17.run(); //run day17
+//        Day18.run(); //run day18
+//        Day19.run(); //run day19
+//        Day20.run(); //run day20
+//        Day21.run(); //run day21
+//        Day22.run(); //run day22
+//        Day23.run(); //run day23
+//        Day24.run(); //run day24
+//        Day25.run(); //run day25
     }
 }
---
+++ AdventOfCode/src/aoc2020/day3/Day3.java
@@ -0,0 +1,42 @@
+package aoc2020.day3;
+
+import aoc2020.Util;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Day3 {
+    public static void run() {
+        File file = new File("day3.txt");
+        ArrayList<ArrayList<Boolean>> forest = new ArrayList<>(); //keep track of the processed objects
+        ArrayList<String> input = Util.readFile(file);
+        for (String s: input) {
+            ArrayList<Boolean> entry = new ArrayList<>();
+            for(char c : s.toCharArray()) {
+                if (c == '.') {
+                    entry.add(false);
+                } else {
+                    entry.add(true);
+                }
+            }
+            forest.add(entry);
+        }
+
+        int res = 1; //MULTIPLIER FLOWS OVER WHEN USING INTEGERS
+        int[][] slopes = {{1,1},{3,1},{5,1},{7,1},{1,2}};
+        for(int[] s : slopes) {
+            int hits = 0;
+            int right = s[0];
+            int down = s[1];
+            for (int i = 0; i < forest.size(); i += (down)) {
+                ArrayList<Boolean> entry = forest.get(i);
+                if (entry.get(((i / down) * right) % entry.size())) {
+                    hits++;
+                }
+            }
+            res *= hits;
+        }
+
+        System.out.println(res);
+    }
+}
