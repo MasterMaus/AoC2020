57b16f0f0e27aedc607459f6dcc6dbcde60be64f
Maurits Hameter <maurits-hameter@hotmail.com>
2020-12-11 13:55:27 +0100

    Solution day 11. getting adjacent chairs needs to be done from chair to direction. not from edge to chair.

--- AdventOfCode/src/aoc2020/day11/Chair.java
+++ AdventOfCode/src/aoc2020/day11/Chair.java
@@ -9,13 +9,18 @@ import java.util.HashSet;
 
 public class Chair {
 
-    private int location[];
+    private int[] location;
+    private int[] grid;
+
+    private HashSet<Chair> allChairs;
     private HashSet<Chair> neighborChairs;
     private boolean occupied;
 
-    public Chair(int[] location, HashSet<Chair> seats) {
+    public Chair(int[] location, HashSet<Chair> seats, int[] grid, boolean occupied) {
         this.location = location;
-        occupied = false;
+        this.grid = grid;
+        this.occupied = occupied;
+        allChairs = seats;
         neighborChairs = getNeighbors(location, seats);
         for(Chair c : neighborChairs) {
             c.addNeighbor(this);
@@ -46,6 +51,18 @@ public class Chair {
         return occupied;
     }
 
+    public static int adjacentOccupied(HashSet<Chair> data) {
+        int res = 0;
+
+        for(Chair chair : data) {
+            if (chair.isOccupied()) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
     public int neighborsOccupied() {
         int res = 0;
         //System.out.println("amount of neighbors: " + neighborChairs.size());
@@ -57,8 +74,96 @@ public class Chair {
         return res;
     }
 
-    public int amountOfNeighbors() {
-        return neighborChairs.size();
+    public HashSet<Chair> getChairsInSight() {
+        HashSet<Chair> res = new HashSet<>();
+        for (int direction = 0; direction < 8; direction++) {
+            Chair c = getChairInSight(direction);
+            if (c!=null) {
+                res.add(c);
+            }
+        }
+        return res;
+    }
+
+    public Chair getChairInSight(int direction) {
+        int y = location[1];
+        int x = location[0];
+        int max_x = grid[0];
+        int max_y = grid[1];
+        Chair res = null;
+        if (direction == 0) { //UP
+            for (int i = 0; i < y; i++) {
+                res = getChairWithLocation(new int[]{x, i}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+            }
+        } else if (direction == 1) { //DOWN
+            for (int i = max_y; i > y; i--) {
+                res = getChairWithLocation(new int[]{x, i}, allChairs);
+                if (res != null) {
+                    return res;               }
+            }
+        } else if (direction == 2) { //LEFT
+            for (int i = 0; i < x; i++) {
+                res = getChairWithLocation(new int[]{i, y}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+            }
+        } else if (direction == 3) {//RIGHT
+            for (int i = max_x; i > x; i--) {
+                res = getChairWithLocation(new int[]{i, y}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+            }
+        } else if (direction == 4) {//UP-LEFT
+            int x2 = x - 1;
+            int y2 = y - 1;
+            while (x2 >= 0 && y2 >= 0) {
+                res = getChairWithLocation(new int[]{x2, y2}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+                x2--;
+                y2--;
+            }
+        } else if (direction == 5) {//UP-RIGHT
+            int x2 = x + 1;
+            int y2 = y - 1;
+            while (x2 <= max_x && y2 >= 0) {
+                res = getChairWithLocation(new int[]{x2, y2}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+                x2++;
+                y2--;
+            }
+        } else if (direction == 6) { //DOWN-LEFT
+            int x2 = x - 1;
+            int y2 = y + 1;
+            while (x2 >= 0 && y2 <= max_y) {
+                res = getChairWithLocation(new int[]{x2, y2}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+                x2--;
+                y2++;
+            }
+        } else if (direction == 7) { //DOWN-RIGHT
+            int x2 = x + 1;
+            int y2 = y + 1;
+            while (x2 <= max_x && y2 <= max_y) {
+                res = getChairWithLocation(new int[]{x2, y2}, allChairs);
+                if (res != null) {
+                    return res;
+                }
+                x2++;
+                y2++;
+            }
+        }
+        return res;
     }
 
 
@@ -104,14 +209,13 @@ public class Chair {
         return res;
     }
 
-    private static Chair getChairWithLocation(int[] loc, HashSet<Chair> seats) {
-        Chair res = null;
+    public static Chair getChairWithLocation(int[] loc, HashSet<Chair> seats) {
         for(Chair c : seats) {
             if (Arrays.equals(loc, c.getLocation())) {
                 return c;
             }
         }
-        return res;
+        return null;
     }
 
 }
--- AdventOfCode/src/aoc2020/day11/Day11.java
+++ AdventOfCode/src/aoc2020/day11/Day11.java
@@ -4,45 +4,67 @@ import aoc2020.Util;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class Day11 {
     public static void run() {
-        File file = new File("day11.txt");
+        File file = new File("day11_2.txt");
         //HashMap<int[], Chair> hall = new HashMap<>(); //keep track of the processed objects
-        HashSet<Chair> seats = new HashSet<Chair>();
+        HashSet<Chair> seats = new HashSet<>();
         ArrayList<String> input = Util.readFile(file);
+        int[] grid = {input.get(0).length(), input.size()};
         for (int y = 0; y < input.size(); y++) {
             String s = input.get(y);
             for (int x = 0; x < s.length(); x++) {
                 int[] loc = {x, y};
                 if (s.charAt(x) == 'L') {
-                    seats.add(new Chair(loc, seats));
+                    seats.add(new Chair(loc, seats, grid, false));
+                } else if (s.charAt(x) == '#') {
+                    seats.add(new Chair(loc, seats, grid, true));
                 }
                 //process the input file into whatever we need
             }
         }
 
-        while (simulationRound(seats) != 0) {
-        }
 
-        int res = 0;
-        for(Chair c : seats) {
-            if (c.isOccupied()) {
-                res++;
+        int i = 0;
+        while (simulationRoundV2(seats) != 0) {
+            i++;
+            int res = 0;
+            for(Chair c : seats) {
+//            System.out.println(c);
+//            System.out.println(c.getChairsInSight().size());
+                if (c.isOccupied()) {
+                    res++;
+                }
             }
-        }
-        System.out.println(res);
+            System.out.println(res);
 
-//        for (int i = 0; i < 10; i++) {
-//            System.out.println(i + ": " + simulationRound(seats));
-//
+            if(i==2) {
+                for(Chair c : seats) {
+                    //System.out.println(c);
+                }
+                break;
+            }
+
+        }
+
+        Chair test = Chair.getChairWithLocation(new int[] {6,0}, seats);
+        HashSet<Chair> testSet = test.getChairsInSight();
+        for (Chair c : testSet) {
+            System.out.println(c);
+        }
+
+//        int res = 0;
+//        for(Chair c : seats) {
+//            if (c.isOccupied()) {
+//                res++;
+//            }
 //        }
+//        System.out.println("ANTWOORD: " + res);
     }
 
-    private static int simulationRound(HashSet<Chair> seats) {
+    private static int simulationRoundV1(HashSet<Chair> seats) {
         HashSet<Chair> changeList = new HashSet<>();
         for(Chair c : seats) {
             if(c.isOccupied()) { //someone is sitting in the seat, swap occupation if there are 4 or more neighbors
@@ -60,6 +82,28 @@ public class Day11 {
             c.swapOccupation();
         }
 
+        return changeList.size();
+    }
+
+    private static int simulationRoundV2(HashSet<Chair> seats) {
+        HashSet<Chair> changeList = new HashSet<>();
+        for(Chair c: seats) {
+            HashSet<Chair> adjacent = c.getChairsInSight();
+            if(c.isOccupied()) { //someone is sitting, swap occupation on rule
+                if(Chair.adjacentOccupied(adjacent) >= 5) {
+                    changeList.add(c);
+                }
+
+            } else { //chair is empty, swap occupation state if all neighbor seats are empty as well
+                if (Chair.adjacentOccupied(adjacent) == 0) {
+                    changeList.add(c);
+                }
+            }
+        }
+
+        for(Chair c : changeList) {
+            c.swapOccupation();
+        }
         return changeList.size();
     }
 }
