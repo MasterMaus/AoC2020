949efcbe72ab6bf2fb0098665c41a644ecbb6c9c
Maurits Hameter <maurits-hameter@hotmail.com>
2020-12-13 09:20:45 +0100

    day13 part 1

--- AdventOfCode/src/aoc2020/day13/Day13.java
+++ AdventOfCode/src/aoc2020/day13/Day13.java
@@ -2,17 +2,29 @@ package aoc2020.day13;
 
 
 import aoc2020.utilities.InputLoader;
+import sun.reflect.generics.tree.Tree;
 
 import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Day13 {
     public static void run() {
-        ArrayList<String/*NewObject*/> newObjectList = new ArrayList<>(); //keep track of the processed objects
+        int timestamp;
+        String[] busses;
+        TreeSet<Integer> timetable = new TreeSet<>();
+        TreeMap<Integer, Integer> earliestDeparture = new TreeMap<>(); // <BUS_ID , TIME TO DEPART>
         ArrayList<String> input = InputLoader.asList("input/day13.txt");
-        for (String s: input) {
-            //process the input file into whatever we need
+        timestamp = Integer.parseInt(input.get(0));
+        busses = input.get(1).replace(",x", "").split(",");
+        for(String s : busses) {
+            timetable.add(Integer.parseInt(s));
         }
-        int res = 0; // STORE RESULT
-        System.out.println(res); //OUTPUT RESULT
+        for (int i : timetable) {
+            earliestDeparture.put(i, i-(timestamp%i));
+        }
+        System.out.println(earliestDeparture.toString());
+//        int res = 0; // STORE RESULT
+//        System.out.println(res); //OUTPUT RESULT
     }
 }
